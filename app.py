4ac22daa7fd318d3895362810a16a7f3a39b4ce0
lukew3 <lukew25073@gmail.com>
2022-04-02 14:15:35 -0400

    layout styling

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
-upload_path = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+upload_path = os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER'])
 
 
 @app.route("/")
@@ -22,13 +22,14 @@ def school_courses(school=None):
 def course_syllabi(school=None, course=None):
     return render_template('course_syllabi.html')
 
-@app.route("/upload", methods=['GET'])
-def upload_form():
-    return render_template('upload_form.html')
-
-@app.route("/submit", methods=['POST'])
-def submit_form():
-    return "<p>Success</p>"
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return render_template('upload_form.html')
+    elif request.method == 'POST':
+        f = request.files['the_file']
+        filename = "example.pdf"
+        f.save(path + filename)
 
 @app.route('/s/<school>/<course>/<filename>')
 def download_file(filename=None):
@@ -37,9 +38,6 @@ def download_file(filename=None):
     # check for file in directory first?
     return send_from_directory(directory=upload_path, filename=filename)
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        f = request.files['the_file']
-        filename = "example.pdf"
-        f.save(path + filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
